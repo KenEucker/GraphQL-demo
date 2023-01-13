d27c51912a4c23be59000f006cb6c6c22f826799
Ken Eucker <keneucker@gmail.com>
2023-01-13 00:48:55 -0800

    Starts over with the db now that things have changed a bit.

--- src/schema/migrations/20221223180039_like_love_repost_share/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Interaction" ADD COLUMN     "love" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "repost" BOOLEAN NOT NULL DEFAULT false;
--- src/schema/migrations/20230102020912_changes_link_to_website/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `link` on the `Author` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Author" DROP COLUMN "link",
-ADD COLUMN     "website" TEXT NOT NULL DEFAULT '';
